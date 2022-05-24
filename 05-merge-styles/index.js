ad03b9f57b566839cacdd85055862e3fbbb534d8
matvey84 <matvey8407@mail.ru>
2022-05-24 22:08:41 +0300

    fix: 5 task, fix function behavior when call function again

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,31 +2,64 @@ const fs = require('fs');
 const path = require('path');
 
 
+
 fs.readdir(path.join(__dirname,'styles'),{withFileTypes:true}, (err, files)=>{
   if(err) (console.log(err));
   const filteredFiles = files.filter(file=> file.isFile())
     .filter(file=>path.extname(file.name) === '.css');
+		fs.access(path.join(__dirname,'project-dist', 'bundle.css'), (err)=>{
+      if(err){
+				filteredFiles.forEach(item=>{
 
-  filteredFiles.forEach(item=>{
+					let readStream = fs.createReadStream(path.join(__dirname, 'styles',`${item.name}`));
+					let writeSteram = fs. createWriteStream(path.join(__dirname,'project-dist', 'bundle.css'), {flags:'a'});
+			
+					readStream
+						.on('error',(err)=>{
+							if(err){
+								writeSteram.destroy();
+								writeSteram.end(console.log('Something wrong....'));
+							}
+						})
+						.pipe(writeSteram)
+						.on('error',(err)=>{
+							if(err){
+								writeSteram.destroy();
+								writeSteram.end(console.log('Something wrong....'));
+							}
+						});
+				});
 
-    let readStream = fs.createReadStream(path.join(__dirname, 'styles',`${item.name}`));
-    let writeSteram = fs. createWriteStream(path.join(__dirname,'project-dist', 'bundle.css'), {flags:'a'});
 
-    readStream
-      .on('error',(err)=>{
-        if(err){
-          writeSteram.destroy();
-          writeSteram.end(console.log('Something wrong....'));
-        }
-      })
-      .pipe(writeSteram)
-      .on('error',(err)=>{
-        if(err){
-          writeSteram.destroy();
-          writeSteram.end(console.log('Something wrong....'));
-        }
-      });
-  });
+			}else{
+
+				fs.rm(path.join(__dirname,'project-dist', 'bundle.css'),/* {recursive: true},*/ (err)=>{
+					if(err){console.log(err);}
+				
+		
+					filteredFiles.forEach(item=>{
+
+						let readStream = fs.createReadStream(path.join(__dirname, 'styles',`${item.name}`));
+						let writeSteram = fs. createWriteStream(path.join(__dirname,'project-dist', 'bundle.css'), {flags:'a'});
+				
+						readStream
+							.on('error',(err)=>{
+								if(err){
+									writeSteram.destroy();
+									writeSteram.end(console.log('Something wrong....'));
+								}
+							})
+							.pipe(writeSteram)
+							.on('error',(err)=>{
+								if(err){
+									writeSteram.destroy();
+									writeSteram.end(console.log('Something wrong....'));
+								}
+							});
+					});
+				});	
+			}
+		}); 
 });
 
 
